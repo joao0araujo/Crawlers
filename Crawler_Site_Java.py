d94c33574e11b0f42ec4ec07cb43213f9e6dc2c7
JoaoAraujo <araujo.vitor2018@gmail.com>
2025-09-17 15:49:03 -0300

    Adicionando dados a uma lista

--- Crawler_Site_Java.py
+++ Crawler_Site_Java.py
@@ -18,4 +18,20 @@ contexto = BeautifulSoup(response.content, 'html.parser')
 
 seleciona_posts = contexto.select(".blog-posts.hfeed.container .post-outer")
 
-print(seleciona_posts[0].select(".post-title.entry-title")[0].text.strip())
+# print(seleciona_posts[0].select(".post-title.entry-title")[0].text.strip())
+
+for post in seleciona_posts:
+    titulo = post.select(".post-title.entry-title a")[0].text.strip()
+    link = post.select(".snippet-fade.r-snippet-fade")[0].get("href")
+    conteudo = post.select(".snippet-item.r-snippetized")[0].text.strip()
+
+    dados = {
+        "titulo": titulo,
+        "link": link,
+        "conteudo": conteudo,
+    }
+
+    lista_de_posts.append(dados)
+
+for i in lista_de_posts:
+    print(i["titulo"])
